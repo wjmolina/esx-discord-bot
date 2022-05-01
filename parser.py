095af37315fdd807582cba3fbce76cdb17880739
Wil <molina.wilfredo.j@gmail.com>
2022-05-01 14:54:07 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -120,7 +120,9 @@ async def read_standings(client, message, args):
 
 async def otherworld(client, message, args):
     args = "_".join(word if word in {"of"} else word.title() for word in args.lower().split(" "))
-    text = requests.get(f"https://otherworld-legends.fandom.com/wiki/{args}").text
+    text = requests.get(f"https://otherworld-legends.fandom.com/wiki/{args}").text.replace(
+        "&#039;", "'"
+    )
     ans = "\n".join(findall(r'<meta property="og:description" content="(.*?)"', text))
     return ans or "I could not find that item."
 
